6a290bd1d4b1be73b661c888461a8623a288e6c1
Joao Porfirio <joaoprf54@gmail.com>
2021-09-22 15:53:44 -0300

    Componente Final

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import '../../css/reset.css';
 import '../../css/style.css';
 import img from "../../assets/img/image.svg";
-
-import google from "../../assets/img/google-icon.png";
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 const Login = () => {
 
   return (
